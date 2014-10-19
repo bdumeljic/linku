30ee6037749605979a39196556ba91f5c8df5cd5
rferizaj <ferizaj.r@gmail.com>
2014-10-19 13:24:10 +0200

    New event
    Implemented create event methods

--- app/src/main/java/com/bdlabs_linku/linku/CreateNewEventActivity.java
+++ app/src/main/java/com/bdlabs_linku/linku/CreateNewEventActivity.java
@@ -3,7 +3,6 @@ package com.bdlabs_linku.linku;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -11,27 +10,28 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
-import android.widget.Toast;
 
 
-public class EventsActivity extends Activity implements EventsFragment.OnFragmentInteractionListener {
+
+public class CreateNewEventActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_events);
+        setContentView(R.layout.activity_create_new_event);
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
-                    .add(R.id.container, new EventsFragment())
+                    .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+        getActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.events, menu);
+        getMenuInflater().inflate(R.menu.create_new_event, menu);
         return true;
     }
 
@@ -41,17 +41,25 @@ public class EventsActivity extends Activity implements EventsFragment.OnFragmen
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.create_event) {
-            Intent intent= new Intent(this,CreateNewEventActivity.class);
-            startActivity(intent);
+        if (id == R.id.action_settings) {
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
 
-    @Override
-    public void onFragmentInteraction(String name) {
-        Toast.makeText(getApplicationContext(), name + ". Great choice!", Toast.LENGTH_SHORT).show();
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_create_new_event, container, false);
+            return rootView;
+        }
     }
 }
